e4a558c42da412b7f2fbc524226e0e73514c8e34
Eva Hottinen <ehottinen@proton.me>
2026-03-21 15:27:17 +0100

    Initial Spring Boot setup with Web, JPA, Security and H2

---
+++ securefiles/src/main/java/com/eva/securefiles/SecurefilesApplication.java
@@ -0,0 +1,13 @@
+package com.eva.securefiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurefilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurefilesApplication.class, args);
+	}
+
+}
